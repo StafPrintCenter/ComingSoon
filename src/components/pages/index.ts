cf7fb0f5b58bd856140a5687771410c0a317695a
steveasterafovo <steveasterafovo@gmail.com>
2026-09-21 13:35:47 +0100

    s

--- src/components/pages/index.ts
+++ src/components/pages/index.ts
@@ -1,8 +1,6 @@
-export { ComingSoonHeader } from "../site/Header";
 export { BuildProgress } from "./BuildProgress";
 export { LivePlatforms } from "./LivePlatforms";
 export { NotifyForm } from "./NotifyForm";
 export { PreviewIllustration } from "./PreviewIllustration";
 export { Roadmap } from "./Roadmap";
 export { ComingSoonHeroText } from "./HeroText";
-export { ComingSoonFooter } from "../site/Footer";
